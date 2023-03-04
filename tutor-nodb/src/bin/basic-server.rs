a53859106922ba554ea9905f2c62a24007d2adbb
Keith Noguchi <keith@noguchi.us>
2023-03-04 15:07:19 -0800

    let's play with actix-web

--- tutor-nodb/src/bin/basic-server.rs
+++ tutor-nodb/src/bin/basic-server.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let app = || App::new().configure(router);
+
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
+
+fn router(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health));
+}
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().json("Hello.  It's up and running")
 }
