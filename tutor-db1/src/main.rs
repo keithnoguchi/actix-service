fe6808ef3d74682f46c1a8e38a33e795a2e9dfe5
Keith Noguchi <keith@noguchi.us>
2023-03-05 12:24:49 -0800

    main: Prepping the application state

--- tutor-db1/src/main.rs
+++ tutor-db1/src/main.rs
@@ -1,13 +1,25 @@
-use tutor_db1::router;
+use actix_web::web;
+use sqlx::postgres::PgPool;
+use tutor_db1::{router, state};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let app = || {
+async fn main() -> eyre::Result<()> {
+    dotenvy::dotenv()?;
+
+    // Prepares the application state.
+    let db_url = std::env::var("DATABASE_URL")?;
+    let db_pool = PgPool::connect(&db_url).await?;
+    let state = web::Data::new(state::State::new(db_pool));
+
+    // Creates a app factory.
+    let app = move || {
         actix_web::App::new()
+            .app_data(state.clone())
             .configure(router::general)
             .configure(router::courses)
     };
 
+    // Starts the server.
     actix_web::HttpServer::new(app)
         .bind("127.0.0.1:3000")?
         .bind("[::1]:3000")?
--- tutor-db1/src/state.rs
+++ tutor-db1/src/state.rs
@@ -2,8 +2,19 @@ use std::sync::atomic::AtomicUsize;
 
 use sqlx::postgres::PgPool;
 
+#[derive(Debug)]
 pub struct State {
     pub health_check_string: String,
     pub visit_count: AtomicUsize,
     pub db: PgPool,
+}
+
+impl State {
+    pub fn new(db: PgPool) -> Self {
+        Self {
+            health_check_string: "I'm good.".to_string(),
+            visit_count: AtomicUsize::new(0),
+            db,
+        }
+    }
 }
