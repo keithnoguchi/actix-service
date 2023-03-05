1718047c7b288c79e686dacda3d794ff586dfad0
Keith Noguchi <keith@noguchi.us>
2023-03-05 11:52:55 -0800

    Add /health endpoint

--- tutor-db1/src/handler.rs
+++ tutor-db1/src/handler.rs
@@ -3,3 +3,12 @@ use actix_web::HttpResponse;
 pub async fn index() -> HttpResponse {
     HttpResponse::Ok().body("Hello world!")
 }
+
+pub async fn health() -> HttpResponse {
+    HttpResponse::Ok().body(
+        r#"
+            <!doctype html>
+            <p>I'm healthy</p>
+        "#,
+    )
+}
--- tutor-db1/src/router.rs
+++ tutor-db1/src/router.rs
@@ -3,5 +3,6 @@ use actix_web::web;
 use crate::handler;
 
 pub fn general(cfg: &mut web::ServiceConfig) {
-    cfg.route("/", web::get().to(handler::index));
+    cfg.route("/", web::get().to(handler::index))
+        .route("/health", web::get().to(handler::health));
 }
