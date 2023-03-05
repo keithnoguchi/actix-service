244510039245309019776b56c679f92edf7dc90a
Keith Noguchi <keith@noguchi.us>
2023-03-05 12:31:19 -0800

    handler: Pass app state to health handler

--- tutor-db1/src/handler.rs
+++ tutor-db1/src/handler.rs
@@ -1,4 +1,7 @@
+use std::sync::atomic::Ordering;
+
 use crate::model::Course;
+use crate::state::State;
 
 use actix_web::{web, HttpResponse};
 
@@ -6,13 +9,15 @@ pub(crate) async fn index() -> HttpResponse {
     HttpResponse::Ok().body("Hello world!")
 }
 
-pub(crate) async fn health() -> HttpResponse {
-    HttpResponse::Ok().body(
+pub(crate) async fn health(state: web::Data<State>) -> HttpResponse {
+    HttpResponse::Ok().body(format!(
         r#"
             <!doctype html>
-            <p>I'm healthy</p>
+            <p>{} visited {} times</p>
         "#,
-    )
+        state.health_check_string,
+        state.visit_count.fetch_add(1, Ordering::Relaxed),
+    ))
 }
 
 pub(crate) async fn new_course(_new_course: web::Json<Course>) -> HttpResponse {
